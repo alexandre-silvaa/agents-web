dfb53eea22b81a0977a72fd40dd0900631695246
alexandre-silvaa <alexandrebsilva9@gmail.com>
2025-07-31 18:38:37 -0300

    feat: wrap routes with QueryClientProvider for react-query integration

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { CreateRoomContainer } from "@/pages/create-room/container";
 import { RoomDetailsContainer } from "@/pages/room-details/container";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<CreateRoomContainer />} index />
-        <Route element={<RoomDetailsContainer />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoomContainer />} index />
+          <Route element={<RoomDetailsContainer />} path="/room/:id" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
